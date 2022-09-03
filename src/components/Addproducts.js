3dfe4ab1c7e845a3040391e688656ed6a9d37b86
jihadrekani <jihadfed95@gmail.com>
2022-09-03 11:17:40 +0300

    add multiple images input to About section

--- src/components/Addproducts.js
+++ src/components/Addproducts.js
@@ -9,9 +9,9 @@ const addProduct=()=>{
 
     return(
       <div className='bg-green-500 flex flex-col items-center justify-center h-screen'>
-          <h2>this page will update with api</h2>
+          <h2>this page will update with backend</h2>
             
-        <h1 className='mb-4 font-bold'> Add Your Products </h1>
+        <h1 className='mb-4 font-bold'> Add new Products </h1>
         <input className='mb-3 rounded pl-1 bg-blue-200 border w-[500px] border-slate-50' type="text"placeholder='Enter product name'
         value={name}onChange={(e)=>{setName(e.target.value)}}
         />
@@ -24,6 +24,7 @@ const addProduct=()=>{
         <input className='mb-3 rounded pl-1 bg-blue-200 border w-[500px] border-slate-50' type="text" placeholder="Enter product company"
         value={company} onChange={(e)=>{setCompany(e.target.value)}}
         />
+        <input className='' type={"file"} multiple ></input>
          <button className="bg-green-700 text-white border border hover:bg-green-500 rounded-xl py-[3px] px-[6px] mt-2" onClick={addProduct}>Add Product</button>
    
    
