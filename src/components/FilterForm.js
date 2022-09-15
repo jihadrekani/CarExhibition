e331d9d321f5172cb4a332d484af3bf9497712b6
jihadrekani <jihadfed95@gmail.com>
2022-09-15 10:43:45 +0300

    filterForm bg changed

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -6,8 +6,8 @@ export default function FilterForm() {
 
 <form className='container  filterform bg-zinc-900 rounded lg:mt-6 xl:mb-[45px] w-[500px] mx-auto  pb-6 mt-[17px] mb-12 grid  place-items-center   '>
 <div className='    w-[320px] h-[260px]  text-center  '>
-  <h2 className='text-lg  text-white text-center font-sans bg-sky-700 rounded-xl mt-2  '>Search our extensive range of quality cars with prices to suit all budgets</h2>
-<select className='rounded w-[250px]  hover:bg-sky-600 mt-2 mb-3 bg-sky-700 text-white border-solid border border-sky-600' id="Any type">
+  <h2 className='text-lg  text-white text-center font-sans bg-red-700 rounded-xl mt-2  '>Search our extensive range of quality cars with prices to suit all budgets</h2>
+<select className='rounded w-[250px]  hover:bg-red-600 mt-2 mb-3 bg-red-700 text-white border-solid border border-sky-600' id="Any type">
   <option value="Any make" defaultValue="selected">Any make</option>
   <option value="Audi">Audi</option>
   <option value="BMW">BMW</option>
@@ -31,7 +31,7 @@ export default function FilterForm() {
 </select>
 
 <br/>
-<select className='w-[250px] rounded mb-3 bg-sky-700  hover:bg-sky-600 text-white border-solid border border-sky-600' id="price">
+<select className='w-[250px] rounded mb-3 bg-red-700  hover:bg-red-600 text-white border-solid border border-sky-600' id="price">
 <option value="price min" defaultValue="selected">price (min)</option>
   <option value="10,000$">from 10,000$</option>
   <option value="audi" >from 20,000$</option>
@@ -55,7 +55,7 @@ export default function FilterForm() {
   <option value=" 10,000$" >from 200,000$</option>
 </select>
 
-<select className='w-[250px] rounded bg-sky-700 text-white border-solid border border-sky-600  hover:bg-sky-600' id="price">
+<select className='w-[250px] rounded bg-red-700 text-white border-solid border border-sky-600  hover:bg-red-600' id="price">
 <option value="price max" defaultValue="selected">price (max)</option>
   <option value="10,000$">from 10,000$</option>
   <option value="audi" >from 20,000$</option>
@@ -79,7 +79,7 @@ export default function FilterForm() {
   <option value=" 10,000$" >from 200,000$</option>
 </select>
 </div>
-<button className='w-[250px] rounded bg-sky-700 text-white mb-3 hover:bg-sky-600 border-solid border border-sky-600'>Search</button>
+<button className='w-[250px] rounded bg-red-700 text-white mb-3 hover:bg-red-600 border-solid border border-sky-600'>Search</button>
 </form>
 
     </div>
--- src/components/MultiSlider.js
+++ src/components/MultiSlider.js
@@ -27,7 +27,7 @@ export default function MultiSlider() {
     <div>
 
         
-    <h2 className='flex justify-center align-center mb-3 text-2xl font-bold'>New Deals <svg xmlns="http://www.w3.org/2000/svg" className=" h-6 w-6 mt-2 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+    <h2 className='flex justify-center align-center mb-4 text-2xl font-bold'>New Deals <svg xmlns="http://www.w3.org/2000/svg" className=" h-6 w-6 mt-2 ml-2" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M17 8l4 4m0 0l-4 4m4-4H3" />
 </svg> </h2>
 
