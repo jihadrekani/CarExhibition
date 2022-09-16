b29e30eff646671fa493d8d83d58f96356796979
jihadrekani <jihadfed95@gmail.com>
2022-09-16 21:35:29 +0300

    FilterForm design and bg changed

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -4,10 +4,10 @@ export default function FilterForm() {
   return (
     <div>
 
-<form className='container  filterform bg-zinc-900 rounded lg:mt-6 xl:mb-[45px] w-[500px] mx-auto  pb-6 mt-[17px] mb-12 grid  place-items-center   '>
+<form className='container  filterform  rounded lg:mt-6 xl:mb-[45px] w-[500px] mx-auto  pb-6 mt-[17px] mb-12 grid  place-items-center   '>
 <div className='    w-[320px] h-[260px]  text-center  '>
-  <h2 className='text-lg  text-white text-center font-sans bg-red-700 rounded-xl mt-2  '>Search our extensive range of quality cars with prices to suit all budgets</h2>
-<select className='rounded w-[250px]  hover:bg-red-600 mt-2 mb-3 bg-red-700 text-white border-solid border border-sky-600' id="Any type">
+  <h2 className='text-lg  text-white text-center font-sans border border-solid  rounded-xl mt-6  '>Search our extensive range of quality cars with prices to suit all budgets</h2>
+<select className='rounded w-[250px]  hover:bg-sky-700 mt-4 mb-2 bg-sky-800 text-white border-solid border border-sky-600' id="filterform">
   <option value="Any make" defaultValue="selected">Any make</option>
   <option value="Audi">Audi</option>
   <option value="BMW">BMW</option>
@@ -31,55 +31,55 @@ export default function FilterForm() {
 </select>
 
 <br/>
-<select className='w-[250px] rounded mb-3 bg-red-700  hover:bg-red-600 text-white border-solid border border-sky-600' id="price">
+<select className='w-[250px] rounded mb-2 bg-sky-800  hover:bg-sky-700 text-white border-solid border border-sky-600' id="price">
 <option value="price min" defaultValue="selected">price (min)</option>
   <option value="10,000$">from 10,000$</option>
-  <option value="audi" >from 20,000$</option>
-  <option value=" 10,000$" >from 30,000$</option>
-  <option value=" 10,000$" >from 40,000$</option>
-  <option value=" 10,000$" >from 50,000$</option>
-  <option value=" 10,000$" >from 60,000$</option>
-  <option value=" 10,000$" >from 70,000$</option>
-  <option value=" 10,000$" >from 80,000$</option>
-  <option value=" 10,000$" >from 90,000$</option>
-  <option value=" 10,000$" >from 100,000$</option>
-  <option value=" 10,000$" >from 110,000$</option>
-  <option value=" 10,000$" >from 120,000$</option>
-  <option value=" 10,000$" >from 130,000$</option>
-  <option value=" 10,000$" >from 140,000$</option>
-  <option value=" 10,000$" >from 150,000$</option>
-  <option value=" 10,000$" >from 160,000$</option>
-  <option value=" 10,000$" >from 170,000$</option>
-  <option value=" 10,000$" >from 180,000$</option>
-  <option value=" 10,000$" >from 190,000$</option>
-  <option value=" 10,000$" >from 200,000$</option>
+  <option value="20,000$" >from 20,000$</option>
+  <option value="30,000$" >from 30,000$</option>
+  <option value="40,000$" >from 40,000$</option>
+  <option value="50,000$" >from 50,000$</option>
+  <option value="60,000$" >from 60,000$</option>
+  <option value="70,000$" >from 70,000$</option>
+  <option value="80,000$" >from 80,000$</option>
+  <option value="90,000$" >from 90,000$</option>
+  <option value="100,000$" >from 100,000$</option>
+  <option value="110,000$" >from 110,000$</option>
+  <option value="120,000$" >from 120,000$</option>
+  <option value="130,000$" >from 130,000$</option>
+  <option value="140,000$" >from 140,000$</option>
+  <option value="150,000$" >from 150,000$</option>
+  <option value="160,000$" >from 160,000$</option>
+  <option value="170,000$" >from 170,000$</option>
+  <option value="180,000$" >from 180,000$</option>
+  <option value="190,000$" >from 190,000$</option>
+  <option value="200,000$" >from 200,000$</option>
 </select>
 
-<select className='w-[250px] rounded bg-red-700 text-white border-solid border border-sky-600  hover:bg-red-600' id="price">
+<select className='w-[250px] mb-2 rounded bg-sky-800 text-white border-solid border border-sky-600  hover:bg-sky-700' id="price">
 <option value="price max" defaultValue="selected">price (max)</option>
   <option value="10,000$">from 10,000$</option>
-  <option value="audi" >from 20,000$</option>
-  <option value=" 10,000$" >from 30,000$</option>
-  <option value=" 10,000$" >from 40,000$</option>
-  <option value=" 10,000$" >from 50,000$</option>
-  <option value=" 10,000$" >from 60,000$</option>
-  <option value=" 10,000$" >from 70,000$</option>
-  <option value=" 10,000$" >from 80,000$</option>
-  <option value=" 10,000$" >from 90,000$</option>
-  <option value=" 10,000$" >from 100,000$</option>
-  <option value=" 10,000$" >from 110,000$</option>
-  <option value=" 10,000$" >from 120,000$</option>
-  <option value=" 10,000$" >from 130,000$</option>
-  <option value=" 10,000$" >from 140,000$</option>
-  <option value=" 10,000$" >from 150,000$</option>
-  <option value=" 10,000$" >from 160,000$</option>
-  <option value=" 10,000$" >from 170,000$</option>
-  <option value=" 10,000$" >from 180,000$</option>
-  <option value=" 10,000$" >from 190,000$</option>
-  <option value=" 10,000$" >from 200,000$</option>
+  <option value="20,000$" >from 20,000$</option>
+  <option value="30,000$" >from 30,000$</option>
+  <option value="40,000$" >from 40,000$</option>
+  <option value="50,000$" >from 50,000$</option>
+  <option value="60,000$" >from 60,000$</option>
+  <option value="70,000$" >from 70,000$</option>
+  <option value="80,000$" >from 80,000$</option>
+  <option value="90,000$" >from 90,000$</option>
+  <option value="100,000$" >from 100,000$</option>
+  <option value="110,000$" >from 110,000$</option>
+  <option value="120,000$" >from 120,000$</option>
+  <option value="130,000$" >from 130,000$</option>
+  <option value="140,000$" >from 140,000$</option>
+  <option value="150,000$" >from 150,000$</option>
+  <option value="160,000$" >from 160,000$</option>
+  <option value="170,000$" >from 170,000$</option>
+  <option value="180,000$" >from 180,000$</option>
+  <option value="190,000$" >from 190,000$</option>
+  <option value="200,000$" >from 200,000$</option>
 </select>
 </div>
-<button className='w-[250px] rounded bg-red-700 text-white mb-3 hover:bg-red-600 border-solid border border-sky-600'>Search</button>
+<button className='w-[100px] rounded bg-sky-800 text-white mb-[110px] hover:bg-sky-700 border-solid border border-sky-600'>Search</button>
 </form>
 
     </div>
